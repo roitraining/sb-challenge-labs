4d6b19440bed409382dd6190aaa8fff5f329ac3b
Jeff Davis <jwdavis@users.noreply.github.com>
2023-11-01 19:58:43 -0700

    branch_summary

--- analyst/raw_queries/branch_summary.sql
+++ analyst/raw_queries/branch_summary.sql
@@ -1,26 +1,26 @@
--- SUMMARY BY BRANCH
---
--- STRATEGY
--- 1. Get count of customers per branch
--- 2. Rank branches by number of customers
--- 3. Get sum of balances per branch
--- 4. Rank branches by total deposits
--- 5. Get count of loans per branch
--- 6. Rank branches by number of loans
--- 7. Get sum of loan amounts per branch
--- 8. Rank branches by total loan amounts
--- 9. Get count of credit cards per branch
--- 10. Rank branches by number of credit cards
--- 11. Join all branch summary tables
---
--- Difficulty: 3/5
+  -- SUMMARY BY BRANCH
+  --
+  -- STRATEGY
+  -- 1. Get count of customers per branch
+  -- 2. Rank branches by number of customers
+  -- 3. Get sum of balances per branch
+  -- 4. Rank branches by total deposits
+  -- 5. Get count of loans per branch
+  -- 6. Rank branches by number of loans
+  -- 7. Get sum of loan amounts per branch
+  -- 8. Rank branches by total loan amounts
+  -- 9. Get count of credit cards per branch
+  -- 10. Rank branches by number of credit cards
+  -- 11. Join all branch summary tables
+  --
+  -- Difficulty: 3/5
 
+CREATE OR REPLACE VIEW `views.branch_summary` AS (
 WITH
   branch_customer_counts AS (
-
-  -- get count of customers per branch
-  -- grouping customers by primary_branch col
-  -- project branch_id and number of customers
+    -- get count of customers per branch
+    -- grouping customers by primary_branch col
+    -- project branch_id and number of customers
   SELECT
     primary_branch AS branch_id,
     COUNT(customer_id) AS num_cust,
@@ -28,25 +28,21 @@ WITH
     `raw.customers`
   GROUP BY
     primary_branch ),
-
   branch_customer_ranks AS (
-
-  -- rank branches by number of customers
-  -- using rank window function
-  -- project branch_id, number of customers, and rank
+    -- rank branches by number of customers
+    -- using rank window function
+    -- project branch_id, number of customers, and rank
   SELECT
     branch_id,
     num_cust,
     RANK() OVER (ORDER BY num_cust DESC ) AS rank
   FROM
     branch_customer_counts),
-
   branch_deposit_sums AS (
-
-  -- get sum of balances per branch
-  -- joining accounts and customers tables
-  -- grouping by primary_branch col
-  -- project branch_id and total deposits
+    -- get sum of balances per branch
+    -- joining accounts and customers tables
+    -- grouping by primary_branch col
+    -- project branch_id and total deposits
   SELECT
     primary_branch AS branch_id,
     SUM(balance) AS total_dep
@@ -58,12 +54,10 @@ WITH
     a.customer_id = c.customer_id
   GROUP BY
     primary_branch ),
-
   branch_deposit_ranks AS (
-
-  -- rank branches by total deposits
-  -- using rank window function
-  -- project branch_id, total deposits, and rank
+    -- rank branches by total deposits
+    -- using rank window function
+    -- project branch_id, total deposits, and rank
   SELECT
     branch_id,
     total_dep,
@@ -71,11 +65,9 @@ WITH
   FROM
     branch_deposit_sums ),
   branch_loan_counts AS (
-
-
-  -- get count of loans per branch
-  -- joining loans, employees, and branches tables
-  -- grouping by branch_id col
+    -- get count of loans per branch
+    -- joining loans, employees, and branches tables
+    -- grouping by branch_id col
   SELECT
     b.branch_id AS branch_id,
     COUNT(*) AS num_loans
@@ -92,11 +84,9 @@ WITH
   GROUP BY
     b.branch_id ),
   branch_loan_ranks AS (
-
-
-  -- rank branches by number of loans
-  -- using rank window function
-  -- project branch_id, number of loans, and rank
+    -- rank branches by number of loans
+    -- using rank window function
+    -- project branch_id, number of loans, and rank
   SELECT
     branch_id,
     num_loans,
@@ -104,12 +94,10 @@ WITH
   FROM
     branch_loan_counts ),
   b_loan_totals AS (
-
-
-  -- get sum of loan amounts per branch
-  -- joining loans, employees, and branches tables
-  -- grouping by branch_id col
-  -- project branch_id and total loan amounts
+    -- get sum of loan amounts per branch
+    -- joining loans, employees, and branches tables
+    -- grouping by branch_id col
+    -- project branch_id and total loan amounts
   SELECT
     b.branch_id AS branch_id,
     SUM(l.loan_amount) AS total_loans
@@ -126,24 +114,20 @@ WITH
   GROUP BY
     b.branch_id ),
   b_loan_totals_rank AS (
-
-  -- rank branches by total loan amounts
-  -- using rank window function
-  -- project branch_id, total loan amounts, and rank
+    -- rank branches by total loan amounts
+    -- using rank window function
+    -- project branch_id, total loan amounts, and rank
   SELECT
     branch_id,
     total_loans,
     RANK() OVER (ORDER BY total_loans DESC ) AS rank
   FROM
     b_loan_totals ),
-
   branch_card_counts AS (
-
-
-  -- get count of credit cards per branch
-  -- joining credit cards and customers tables
-  -- grouping by branch_id col
-  -- project branch_id and number of credit cards
+    -- get count of credit cards per branch
+    -- joining credit cards and customers tables
+    -- grouping by branch_id col
+    -- project branch_id and number of credit cards
   SELECT
     c.primary_branch AS branch_id,
     COUNT(*) AS num_cards
@@ -155,21 +139,18 @@ WITH
     cc.customer_id = c.customer_id
   GROUP BY
     branch_id ),
-
   branch_card_ranks AS (
-
-  -- rank branches by number of credit cards
-  -- using rank window function
-  -- project branch_id, number of credit cards, and rank
+    -- rank branches by number of credit cards
+    -- using rank window function
+    -- project branch_id, number of credit cards, and rank
   SELECT
     branch_id,
     num_cards,
     RANK() OVER (ORDER BY num_cards DESC ) AS rank
   FROM
     branch_card_counts )
-
--- join all branch summary tables
--- project branch cols and count cols and rank cols
+  -- join all branch summary tables
+  -- project branch cols and count cols and rank cols
 SELECT
   b.*,
   c.num_cust,
@@ -203,4 +184,4 @@ ON
 JOIN
   branch_card_ranks cc
 ON
-  c.branch_id = cc.branch_id
+  c.branch_id = cc.branch_id)
