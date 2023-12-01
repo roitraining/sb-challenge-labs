2fb6aadd77373c70c982c3721742ea63900bcf07
Jeff Davis <jwdavis@users.noreply.github.com>
2023-11-30 21:38:47 -0800

    percentile

--- analyst/functions/genait_setup.sql
+++ analyst/functions/genait_setup.sql
@@ -1,8 +1,3 @@
-  CREATE SCHEMA
-    `models` OPTIONS ( description = 'BQML Models',
-      location = 'US');
-
-  
 CREATE OR REPLACE MODEL
   `models.upsell` REMOTE
 WITH CONNECTION `us.upsell`  OPTIONS(ENDPOINT = 'text-bison@001')
